5c8e337b00a16e053e6c878068aff87af8c849c7
Firmiana-Unity <122654548+FirmianaI7z9@users.noreply.github.com>
2024-01-17 20:02:01 +0900

    Update v1.4.1

--- rank/js/rank.js
+++ rank/js/rank.js
@@ -26,6 +26,8 @@ function displayRank(kind, max = -1) {
   ele = clone;
 
   let cnt = 1;
+  let cur_val = 0;
+  let cur_rank = 0;
   const wait_get_rank = (async() => {
     if (rank_save[kind] == null) {
       rank_save[kind] = await getRank(kind);
@@ -33,10 +35,20 @@ function displayRank(kind, max = -1) {
     rank_save[kind].forEach((doc) => {
       var i = item.cloneNode(true);
       i.style = "";
-      if (cnt <= 3) i.className = `rank_item rank_${cnt}`;
-      i.querySelector('.rank_num').innerText = `${cnt}`;
-      if (cnt <= 3) i.querySelector('.rank_num').className = `rank_num rank_${cnt}_text`;
+
+      let r = cnt;
+      if (doc['score'] == cur_val) r = cur_rank;
+      else {
+        cur_rank = r;
+        cur_val = doc['score'];
+      }
+
+      if (r <= 3) i.className = `rank_item rank_${r}`;
+      i.querySelector('.rank_num').innerText = `${r}`;
+
+      if (r <= 3) i.querySelector('.rank_num').className = `rank_num rank_${r}_text`;
       i.querySelector('.rank_inner').querySelector('.rank_name').innerText = doc['name'];
+
       if (doc['score'] == (max == -1 ? 5001 * Number(kind.split('_')[1]) : max)) {
         i.querySelector('.rank_inner').querySelector('.rank_name').style = "color:#608;";
         i.querySelector('.rank_inner').querySelector('.rank_score').style = "color:#608;";
@@ -45,9 +57,11 @@ function displayRank(kind, max = -1) {
         i.querySelector('.rank_inner').querySelector('.rank_name').style = "color:#f00;";
         i.querySelector('.rank_inner').querySelector('.rank_score').style = "color:#f00;";
       }
+
       i.querySelector('.rank_inner').querySelector('.rank_score').innerHTML = doc['score'] + (max == -1 ? ' pts.' : ' 問正解') +
         (doc['score'] == (max == -1 ? 5001 * Number(kind.split('_')[1]) : max) ? '<span class="title_tag" style="color:#fff;background-color:#608;">完全制覇</span>' : 
         (doc['score'] >= (max == -1 ? 5000 * Number(kind.split('_')[1]) : max * 0.9) ? '<span class="title_tag" style="color:#fff;background-color:#f00;">達人</span>' : ""));
+      
       ele.appendChild(i);
       cnt++;
     });
